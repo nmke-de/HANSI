f9c0616498d959814cb3ea99bca4de1056d19d36
nmke-de <github@nmke.de>
2022-08-08 00:47:32 +0200

    First JS

---
+++ hansi.js
@@ -0,0 +1,10 @@
+
+const Q = document.getElementById;
+
+window.addEventListener("load", () => {
+	Q("hitpoints").addEventListener("input", updateHitpoints);
+});
+
+let updateHitpoints = () => {
+	// TODO stuff comes here.
+};
