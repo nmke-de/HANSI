5de29814e6dea123a1bcee293a5e9d2fd3c70a18
nmke-de <github@nmke.de>
2022-08-08 01:45:31 +0200

    Added preliminary newCharacter() function

--- hansi.js
+++ hansi.js
@@ -5,6 +5,14 @@ window.addEventListener("load", () => {
 	Q("hitpoints").addEventListener("input", updateHitpoints);
 });
 
-let updateHitpoints = (ev) => {
+const updateHitpoints = (ev) => {
 	Q("hitpoints-in-digits").innerHTML = ev.target.value;
+};
+
+const newCharacter = () => {
+	// TODO do more than a demo here!
+	let dialog = document.createElement("dialog");
+	dialog.innerHTML = "<form method='dialog'><button>Ok</button></form>";
+	Q("sheet").appendChild(dialog);
+	dialog.show();
 };
